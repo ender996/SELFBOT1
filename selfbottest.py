13c3beee98f360cf432549e8cee50be21ca60663
ender996 <45601362+ender996@users.noreply.github.com>
2019-01-08 07:17:09 -0500

    Update selfbottest.py

--- selfbottest.py
+++ selfbottest.py
@@ -455,7 +455,7 @@ def dhenzaBot(op):
                 msg_id = msg.id
                 receiver = msg.to
                 sender = msg._from
-                if msg.text in ["Help"]:
+                if msg.text in ["/Help"]:
                     dz.sendMessage(msg.to,helpMessage)          
 					
         if op.type == 25:
@@ -718,7 +718,7 @@ def dhenzaBot(op):
                     elif 'Spamtag @' in msg.text:
                         dz.sendMessage(msg.to,msg.text,msg.contentMetadata)
                     elif '/help' in text.lower():
-                        dz.sendMessage(msg.to,"Help")
+                        dz.sendMessage(msg.to,"/Help")
                     elif '/reboot' in text.lower():
                         dz.sendMessage(msg.to,"Reboot")
                     elif '/member list' in text.lower():
