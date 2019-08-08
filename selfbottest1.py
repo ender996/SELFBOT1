8cb9a3af34e11349569bcba4a65196c0eb51aa02
ender996 <45601362+ender996@users.noreply.github.com>
2019-08-08 13:50:27 -0400

    Update selfbottest1.py

--- selfbottest1.py
+++ selfbottest1.py
@@ -935,6 +935,8 @@ def dhenzaBot(op):
                         dz.sendMessage(msg.to,msg.text)
                     elif "testgroup: " in text.lower():
                         dz.sendMessage(msg.to,msg.text)
+                    elif "/bus2" in text.lower():
+                        dz.sendImageWithURL(msg.to,"https://darklegendsgaming.com/buspic")
                     else:
                         pass
                 else:
